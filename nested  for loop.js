66768f72a97e4bde9bfa6894c16316fd025b195a
programming10000 <dutycycle480@gmail.com>
2023-03-14 21:42:21 +0300

    nested loop

---
+++ nested  for loop.js
@@ -0,0 +1,6 @@
+for (var i=0;i<=10;i++){
+    for(var j=0;j<=10;j++){
+        console.log(j);
+    }
+    console.log(i);
+}
---
+++ switch.js
@@ -0,0 +1,11 @@
+let i=0;
+switch (i>0) {
+    case 1:
+  console.log('monday');
+      break;
+    case 2:
+        console.log('tusday');
+      break;
+    default:
+        console.log('sunday');
+  }
